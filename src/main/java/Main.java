98e611507ca0a90785f802ad36a7727730fd311b
jbeck18 <j.beck18@me.com>
2019-11-20 12:34:28 -0500

    Added files

---
+++ src/main/java/Main.java
@@ -0,0 +1,118 @@
+import com.mongodb.BulkUpdateRequestBuilder;
+import com.mongodb.BulkWriteOperation;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.BsonObjectId;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+import parser.Parser;
+
+public class Main {
+
+    static String beforeJson = "{\n"
+            + "    'a': {\n"
+            + "        'b': {\n"
+            + "            'c': {\n"
+            + "                'd': 1\n"
+            + "            }\n"
+            + "        },\n"
+            + "        'g': 2\n"
+            + "    },\n"
+            + "    'e': 3,\n"
+            + "    'f': [4,5,6]\n"
+            + "}";
+
+    static String afterJson = "{\n"
+            + "    'a': {\n"
+            + "        'b': {\n"
+            + "            'c': {\n"
+            + "                'd': 10\n"
+            + "            }\n"
+            + "        },\n"
+            + "        'g': 20\n"
+            + "    },\n"
+            + "    'e': {\n"
+            + "        'z': [1]\n"
+            + "    },\n"
+            + "    'f': [4,6]\n"
+            + "}";
+
+    public static void main(String[] args) {
+        // runBenchmark();
+        // System.out.println(Parser.parse(BsonDocument.parse(beforeJson), BsonDocument.parse(afterJson)));
+
+        BsonDocument before = new BsonDocument();
+        BsonDocument after = new BsonDocument();
+
+        for (int j = 0; j < 1000; j++) {
+            for (int i = 0; i < 50_000; i++) {
+                before.put(i+"", new BsonInt32(i));
+                after.put(i+"", new BsonInt32(i+1));
+            }
+        }
+        long[] times = new long[1000];
+        for (int i = 0; i < times.length; i++) {
+            long start = System.currentTimeMillis();
+            Parser.parse(before, after);
+            long end = System.currentTimeMillis();
+            times[i] = end-start;
+        }
+        Parser.executor.shutdown();
+
+        //BsonDocument result = Parser.parse(before, after);
+        //System.out.println(result.toJson());
+        //System.out.println(result.keySet().size());
+
+        int sum = 0;
+        for (int i = 0; i < times.length; i++) {
+            sum += times[i];
+        }
+        System.out.println(sum/times.length);
+    }
+
+    private static void runBenchmark() {
+        Document[] updates = new Document[1_000_000];
+        System.out.println("Generating updates...");
+        for (int i = 0; i < updates.length; i++) {
+            updates[i] = new Document().append("$set", getRandomUpdate());
+        }
+
+        System.out.println("Applying updates...");
+        long start = System.currentTimeMillis();
+        long middle = start;
+        for (int i = 0; i < updates.length; i++) {
+            Operations.updateOne(new Document().append("_id", new BsonObjectId(new ObjectId("5dd42316d34e26759027f476"))), updates[i]);
+
+            if (i % 2000 == 0) {
+                long now = System.currentTimeMillis();
+                System.out.println("2000 processed in " + (now-middle)/1000 + " seconds");
+                middle = now;
+            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Run Time: " + (end-start)/1000.0  + " seconds");
+    }
+
+    static String[] keys = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"};
+    static Random random = new Random(System.currentTimeMillis());
+    private static String getRandomKey() {
+        return keys[random.nextInt(keys.length)];
+    }
+
+    private static Document getRandomUpdate() {
+        int numOfUpdates = random.nextInt(6) + 1;
+        Document result = new Document();
+        for(int i = 0; i < numOfUpdates; i++) {
+            String key = getRandomKey();
+            while (result.containsKey(key)) {
+                key = getRandomKey();
+            }
+            result.put(key, new BsonInt32(random.nextInt(10000)+1));
+        }
+        return result;
+    }
+}
---
+++ src/main/java/parser/Parser.java
@@ -0,0 +1,140 @@
+package parser;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonInt32;
+import org.bson.BsonValue;
+
+/**
+ * A naive strategy to compute the difference between two documents
+ */
+public class Parser {
+
+    private static final int WORK_UNIT_SIZE = 8192;
+    public static final ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(4);
+
+    private static BsonDocument diff;
+
+    /**
+     * Computes the diff between two bson documents
+     * @param before The bson document before modification
+     * @param after The bson document after modification
+     * @return A document containing the difference between the provided documents
+     */
+    public static BsonDocument parse(BsonDocument before, BsonDocument after) {
+        diff = new BsonDocument();
+        computeDiffDict(before, after, diff);
+        return diff;
+    }
+
+    /**
+     * Computes the difference between two bson values
+     */
+    private static void computeDiff(BsonValue before, BsonValue after, BsonDocument diff, String key) {
+        if (Objects.equals(before, after)) {
+            return;
+        }
+
+        if (before instanceof BsonDocument && after instanceof BsonDocument) {
+            if (!diff.containsKey(key)) {
+                diff.put(key, new BsonDocument());
+            }
+            computeDiffDict(before.asDocument(), after.asDocument(), diff.getDocument(key));
+        } else if (before instanceof BsonArray && after instanceof BsonArray) {
+            if (!diff.containsKey(key)) {
+                diff.put(key, new BsonDocument());
+            }
+            computeDiffList(before.asArray(), after.asArray(), diff.getDocument(key), key);
+        } else {
+            //synchronized (diff) {
+                diff.put(key, before);
+            //}
+        }
+    }
+
+    /**
+     * Computes the difference between two bson documents
+     */
+    private static void computeDiffDict(BsonDocument before, BsonDocument after, BsonDocument diff) {
+        Set<String> keys = new HashSet<>();
+        keys.addAll(before.keySet());
+        keys.addAll(after.keySet());
+
+        /*
+        if (keys.size() >= WORK_UNIT_SIZE) {
+            Iterator<String> itr = keys.iterator();
+            List<Set<String>> partitions = new ArrayList<>();
+            partitions.add(new HashSet<>());
+            final CountDownLatch latch = new CountDownLatch((keys.size() / WORK_UNIT_SIZE));
+
+            int iteration = 0;
+            while (itr.hasNext()) {
+                if (partitions.get(iteration).size() < WORK_UNIT_SIZE) {
+                    partitions.get(iteration).add(itr.next());
+                }
+
+                if  (partitions.get(iteration).size() == WORK_UNIT_SIZE || !itr.hasNext()) {
+                    int finalIteration = iteration;
+                    executor.submit(() -> {
+                        for (String key : partitions.get(finalIteration)) {
+                            computeDiff(before.get(key), after.get(key), diff, key);
+                        }
+                        latch.countDown();
+                    });
+                    partitions.add(new HashSet<>());
+                    iteration++;
+                }
+            }
+
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            return;
+        }
+
+         */
+
+        for (String key : keys) {
+            BsonValue old = before.get(key);
+            BsonValue curr = after.get(key);
+
+            computeDiff(old, curr, diff, key);
+        }
+    }
+
+    /**
+     * Computes difference between two bson arrays
+     */
+    private static void computeDiffList(BsonArray before, BsonArray after, BsonDocument diff, String key) {
+        int maxLength = Math.max(before.size(), after.size());
+        BsonArray result = new BsonArray();
+
+        for (int i = 0; i < maxLength; i++) {
+            BsonValue old = i < before.size() ? before.get(i) : null;
+            BsonValue curr = i < after.size() ? after.get(i) : null;
+
+            if (Objects.equals(old, curr)) {
+                continue;
+            }
+
+            BsonDocument elemDiff = new BsonDocument();
+            computeDiff(old, curr, elemDiff, key);
+
+            BsonDocument obj = new BsonDocument();
+            obj.append("index", new BsonInt32(i));
+            obj.append("diff", elemDiff.getOrDefault(key, elemDiff));
+
+            result.add(obj);
+        }
+
+        diff.put(key, result);
+    }
+
+}
